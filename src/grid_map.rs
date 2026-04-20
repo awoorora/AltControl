58a403cfb40b876de1180eb4174fe02ba4a31de2
cuhleen_ <petreanu.calin.plus@gmail.com>
2026-04-20 08:59:09 +0300

    Actual movement
    Simple movement on two keys done

--- src/grid_map.rs
+++ src/grid_map.rs
@@ -1,3 +1,96 @@
-// pub const GRID_COORDS: [(char, (f32, f32)); 30] = [
-//     ('q', (0.1, 0.1)), ('w', (0.2, 0.1))
-// ]
+use rdev::Key;
+use std::collections::HashMap;
+use std::sync::OnceLock;
+
+/**
+El mucho texto
+
+On Alt+Left Ctrl key combo, open up an overlay. This overlay is a grid as follows:
+First row: AQ SQ DQ ... LQ ;Q
+Second row: AW SW DW ... LW ;W
+...
+8th row: AI SI DI ... LI ;I
+...
+Last row: A/ S/ D/ ... ;/
+
+This means 10 columns and 30 rows.
+On a 1920x1080 screen, a cell would take up 192x36 pixels. When moving to a cell, the mouse will move in the middle of it. Assuming each cell is mapped to an X and Y coordinate, [A B] means column A will point to x = 0 * 192, row B means y = 24 * 30 (remember! alphabetical order != qwerty order. Also remember! Only the middle keyboard row (a, s, ..., l, ;) have valid x coords! If the user first inputs a key without an x coord, exit overlay, write a message in console at most)
+
+Basically the rows follow the middle row of the qwerty keyboard (A S D F ... L ;) and the columns follow the entire qwerty layout (q w e r ... p a s ... l ; z x ... m , . /), with Left Ctrl as Left Click and Alt as Right Click
+Once the overlay is open, input from the user goes like this:
+Optional: The user presses Left Ctrl or Alt to click where the mouse is already at, overlay closes, back to monitoring mode
+The user pressed D: the mouse moves to the middle of the D column.
+Optional: The user presses Left Ctrl or Alt to click where the mouse is already at (middle of D column), overlay closes, back to monitoring mode
+The user presses W: the mouse moves to the specific DW cell, in the middle of it, somewhere in the top left
+The user presses Left Ctrl or Alt to click where the mouse is already at (DW cell), overlay closes, back to monitoring mode
+
+If the user presses any key on the left side of the qwerty keyboard, the mouse moves to the center of the first third of the cell. If the user presses any key on the right side of the qwerty keyboard, the mouse moves to the center of the last third of the cell. If the user presses space, the mouse remains in the center of the cell. This assumes the cells are "split" into thirds, one row, three columns, though this is not shown, as it would clutter an already cluttered screen
+
+The overlay automatically closes itself.
+
+Esc closes the overlay
+A non-column key pressed at overlay start closes the overlay
+Unrecognized keys close the overlay
+*/
+
+    pub struct GridEngine{
+        pub screen_w: f32,
+        pub screen_h: f32
+    }
+
+    static COL_MAP: OnceLock<HashMap<Key, usize>> = OnceLock::new();
+    static ROW_MAP: OnceLock<HashMap<Key, usize>> = OnceLock::new();
+
+    impl GridEngine{
+        pub fn new(w: f32, h: f32) -> Self{
+            Self::init_maps();
+            GridEngine{screen_w: w, screen_h: h}
+        }
+
+        fn init_maps() {
+            ROW_MAP.get_or_init(|| {
+                let mut m = HashMap::new();
+                let row_selector_keys = vec![
+                    // QWERTY
+                    Key::KeyQ, Key::KeyW, Key::KeyE, Key::KeyR, Key::KeyT, Key::KeyY, Key::KeyU, Key::KeyI, Key::KeyO, Key::KeyP,
+                    // ASDF
+                    Key::KeyA, Key::KeyS, Key::KeyD, Key::KeyF, Key::KeyG, Key::KeyH, Key::KeyJ, Key::KeyK, Key::KeyL, Key::SemiColon,
+                    // ZXCV
+                    Key::KeyZ, Key::KeyX, Key::KeyC, Key::KeyV, Key::KeyB, Key::KeyN, Key::KeyM, Key::Comma, Key::Dot, Key::Slash
+                ];
+                for (index, key) in row_selector_keys.into_iter().enumerate() {
+                    m.insert(key, index);
+                }
+                m
+            });
+
+            COL_MAP.get_or_init(|| {
+                let mut m = HashMap::new();
+                let col_selector_keys = vec![
+                    Key::KeyA, Key::KeyS, Key::KeyD, Key::KeyF, Key::KeyG,
+                    Key::KeyH, Key::KeyJ, Key::KeyK, Key::KeyL, Key::SemiColon
+                ];
+                for (index, key) in col_selector_keys.into_iter().enumerate() {
+                    m.insert(key, index);
+                }
+                m
+            });
+        }
+
+        pub fn get_coords(&self, col_key: Key, row_key: Key) -> Option<(f32, f32)>{
+            let col_idx = COL_MAP.get()?.get(&col_key)?;
+            let row_idx = ROW_MAP.get()?.get(&row_key)?;
+
+            println!("DEBUG: Col Key: {:?} -> Index: {:?}, Row Key: {:?} -> Index: {:?}", col_key, col_idx, row_key, row_idx);
+
+            let cell_w = self.screen_w / 10.0;
+            let cell_h = self.screen_h / 30.0;
+
+            println!("DEBUG: Cell W: {}, Cell H: {}", cell_w, cell_h);
+
+            let x = (*col_idx as f32 * cell_w) + (cell_w / 2.0);
+            let y = (*row_idx as f32 * cell_h) + (cell_h / 2.0);
+
+            Some((x,y))
+        }
+    }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,43 @@
 mod grid_map;
 
 use std::collections::HashSet;
+use std::mem::swap;
 use std::sync::{Mutex, OnceLock};
 use rdev::{Event, listen, EventType, Key, display_size};
 use crate::AppMode::{Monitoring, Navigating};
+use crate::grid_map::GridEngine;
 
-#[derive(Debug)]
+// TODO: REMOVE!
+// Check winit's monitor methods or the dpi crate
+const magic_scaling_number: f32 = 1.25;
+
 enum AppMode{
     Monitoring,
     Navigating
 }
 
-#[derive(Debug)]
+
 struct AppState{
     keys_history: Vec<Key>,
     keys_current: HashSet<Key>,
-    app_mode:AppMode
+    app_mode:AppMode,
+    grid_engine: GridEngine
 }
 
 static STATE: OnceLock<Mutex<AppState>> = OnceLock::new();
 
 fn get_state() -> &'static Mutex <AppState> {
     STATE.get_or_init(|| {
+        let (w, h) = display_size().unwrap();
+
+        let w = w as f32;
+        let h = h as f32;
         Mutex::new(AppState{
             keys_history: Vec::new(),
             keys_current: HashSet::new(),
-            app_mode:Monitoring
+            app_mode:Monitoring,
+
+            grid_engine: GridEngine::new(w * magic_scaling_number, h * magic_scaling_number)
         })
     })
 }
@@ -58,18 +70,46 @@ fn handle_monitoring(event: Event, state: &mut AppState){
     }
 }
 
-fn handle_navigation(event: Event, state: &mut AppState){
+fn handle_navigation(event: Event, state: &mut AppState) {
     match event.event_type {
         EventType::KeyPress(key) => {
-            if state.keys_current.contains(&key) {
-                return;
-            }
+            if state.keys_current.contains(&key) { return; }
             state.keys_current.insert(key);
-        }
-        EventType::KeyRelease(key) => {
-            state.keys_current.clear();
+            state.keys_history.push(key);
 
-            if key == Key::Escape{
+            println!("DEBUG: A key has been pressed key history: {:?}", state.keys_history);
+            
+            if state.keys_history.len() >= 2 {
+                println!("DEBUG: Two keys in history, attempting to get their coords");
+                let col_selector_key = state.keys_history[0];
+                let row_selector_key = state.keys_history[1];
+
+                if let Some((x, y)) = state.grid_engine.get_coords(col_selector_key, row_selector_key) {
+                    std::thread::spawn(move || {
+                        let actual_x = x / magic_scaling_number;
+                        let actual_y = y / magic_scaling_number;
+                        if let Err(e) = rdev::simulate(&EventType::MouseMove { x: actual_x as f64, y: actual_y as f64 }) {
+                            eprintln!("Failed to simulate mouse move: {:?}", e);
+                        } else {
+                            println!("Moved to: {}, {}", x, y);
+                        }
+                    });
+                } else {
+                    println!("Invalid Grid Selection!");
+                }
+
+                println!("DEBUG: Attempt finished successfully");
+
+                state.keys_current.clear();
+                state.keys_history.clear();
+                state.app_mode = Monitoring;
+                println!("Overlay Closed");
+            }
+        }
+
+        EventType::KeyRelease(key) => {
+            state.keys_current.remove(&key);
+            if key == Key::Escape {
                 state.keys_history.clear();
                 state.app_mode = Monitoring;
                 println!("Overlay Closed");
@@ -103,14 +143,10 @@ fn main(){
     let w = w as f32;
     let h = h as f32;
 
-    // TODO: remove the magic number for scaling
-    // Check winit's monitor methods or the dpi crate
-    println!("The screen has {:.0}x{:.0} pixels", w * 1.25, h * 1.25); // Windows Scaled at 125%
+    println!("The screen has {:.0}x{:.0} pixels", w * magic_scaling_number, h * magic_scaling_number); // Windows Scaled at 125%
     if let Err(error) = listen(callback) {
         println!("Error: {:?}", error)
     }
 }
 
 // TODO: Alt+Ctrl might mess up stuff in the current app the user has open. Find a way to fix it, or just put up a disclaimer
-// TODO: big grid_map.rs, currently empty
-// TODO: actual math based on keys pressed
